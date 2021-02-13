dcc1665d9603f945034ed4f57efae51e39fe5524
vina97 <vinai@hotmail.it>
2021-02-13 14:41:22 +0100

    fix script + add statistics images

--- Statistics/plot_graph.py
+++ Statistics/plot_graph.py
@@ -4,26 +4,34 @@ Pandas and matplotlib libraries are required
 '''
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
-data = pd.read_csv('log.txt', sep='\s+')
+# Here set expected log path
+data = pd.read_csv('Statistics/lockdown_log.txt', sep='\t', engine="python")
+# Due to long log file, parse it with fixed offset
 data = pd.DataFrame(data).iloc[::100, :]
 
 plt.style.use('ggplot')
 plt.xlabel('Time')
 plt.ylabel("Number")
 
+# time parsing for xtick feature
+k = 10
+
+xoffset = []
 xlabel = []
-for i in data['MinutesPassed'][::10]:
-    integer = int(i.split(",")[0])
-    ndays = int(integer/1440)
-    nhours = int((integer % 1440)/60)
-    minutes = (integer % 1440) % 60
+for i in data['MinutesPassed'][::k]:
+    # Transform minutes time into more readable format
+    value = int(i)
+    ndays = int(value/1440)
+    nhours = int((value % 1440)/60)
+    minutes = (value % 1440) % 60
     date = f"{ndays}D {nhours}H {minutes}m"
+    xoffset.append(i)
     xlabel.append(date)
 
-plt.xticks(range(0, len(data['MinutesPassed']), 10),
-           xlabel, rotation="vertical")
 
+# plot everything
 plt.plot(data['MinutesPassed'], data['Population'], label="Total Population")
 plt.plot(data['MinutesPassed'], data['Exposed'], label="Exposed")
 plt.plot(data['MinutesPassed'], data['TotalExposed'], label="Total Exposed")
@@ -33,7 +41,9 @@ plt.plot(data['MinutesPassed'], data['Death'], label="Death")
 plt.plot(data['MinutesPassed'], data['Recovered'], label="Recovered")
 plt.plot(data['MinutesPassed'], data['TotalRecovered'],
          label="Total Recovered")
-plt.title("Data analysis without lockdown")
+# set title
+plt.title("Data analysis with lockdown")
+plt.xticks(xoffset, xlabel, rotation='vertical')
 plt.legend()
 plt.grid(True)
 plt.show()
