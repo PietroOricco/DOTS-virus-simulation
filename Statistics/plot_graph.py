95f8d0c41b4b5dee620676fcbc3c9137d62958f2
vina97 <vinai@hotmail.it>
2021-02-14 17:11:54 +0100

    last log plotted

--- Statistics/plot_graph.py
+++ Statistics/plot_graph.py
@@ -7,7 +7,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Here set expected log path
-data = pd.read_csv('Statistics/lockdown_log.txt', sep='\t', engine="python")
+data = pd.read_csv('Statistics/log_no_lockdown_30000.txt',
+                   sep='\t', engine="python")
 # Due to long log file, parse it with fixed offset
 data = pd.DataFrame(data).iloc[::100, :]
 
@@ -16,7 +17,7 @@ plt.xlabel('Time')
 plt.ylabel("Number")
 
 # time parsing for xtick feature
-k = 10
+k = 6
 
 xoffset = []
 xlabel = []
@@ -39,10 +40,9 @@ plt.plot(data['MinutesPassed'], data['Symptomatic'], label="Symptomatic")
 plt.plot(data['MinutesPassed'], data['Asymptomatic'], label="Asymptomatic")
 plt.plot(data['MinutesPassed'], data['Death'], label="Death")
 plt.plot(data['MinutesPassed'], data['Recovered'], label="Recovered")
-plt.plot(data['MinutesPassed'], data['TotalRecovered'],
-         label="Total Recovered")
+#plt.plot(data['MinutesPassed'], data['TotalRecovered'],         label="Total Recovered")
 # set title
-plt.title("Data analysis with lockdown")
+plt.title("Data analysis without lockdown")
 plt.xticks(xoffset, xlabel, rotation='vertical')
 plt.legend()
 plt.grid(True)
